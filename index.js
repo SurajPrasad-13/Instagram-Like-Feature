b9c53488ad3e23083d1b80b0b2a713abfb31c108
SurajPrasad-13 <spyadav0413@gmail.com>
2025-02-17 21:08:50 +0530

    add rotation

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ let con =  document.querySelector('.container')
    let icon =  document.querySelector('i')
 
    con.addEventListener('dblclick',function(){
-    icon.style.transform = 'translate(-50%,-50%) scale(1)'
+    icon.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
     icon.style.opacity = 0.8
     console.log('hello')
     setTimeout(() => {
      icon.style.opacity = 0
     }, 900);
     setTimeout(() => {
-      icon.style.transform = 'translate(-50%,-50%) scale(0)'
+      icon.style.transform = 'translate(-50%,-50%) scale(0) rotate(-50deg)'
     }, 1200);
    })
